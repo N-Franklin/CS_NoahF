6bd237e77dabdf104e7b752f5372674954eb28b1
N-Franklin <nmfranklin@gmail.com>
2017-04-12 10:27:23 -0700

    Fixed some bugs, still several

--- Trinket/Connect_4/Board.py
+++ Trinket/Connect_4/Board.py
@@ -1,76 +1,4 @@
 from random import randint
-
-class Player:
-    def __init__(self, ox, tbt, ply):
-        self.ox=ox
-        self.tbt=tbt
-        self.ply=ply
-
-    def __repr__(self):
-        """ creates an appropriate string """
-        s = "Player for " + self.ox + "\n"
-        s += "  with tiebreak type: " + self.tbt + "\n"
-        s += "  and ply == " + str(self.ply) + "\n\n"
-        return s
-    def opp(self):
-        if self.ox=='X':
-            return 'O'
-        elif self.ox=='O':
-            return 'X'
-    def scoreBoard(self, Board):
-        z=self.opp()
-        if Board.winsFor(self.ox)==True:
-            return 100
-        elif Board.winsFor(z)==True:
-            return 0
-        elif Board.isFull()==True:
-            return 1
-        else:
-            return 50
-    def findScore(self,Board):
-        scores=[]
-        for Col in range(0,Board.width):
-            b=Board
-            b.addmove(Col,self.ox)
-            scores.append(self.scoreBoard(Board))
-        return scores
-    def highScores(self,scores,board):
-        k=[]
-        b=0
-        for i in range(0,scores.length):
-            if scores[i]>b and self.foresight(board,i):
-                b=scores[i]
-                k=[]
-
-                k.append(i)
-            elif scores[i]==b and self.foresight(board,i):
-                k.append(i)
-        return k
-
-    def tiebreakMove(self,board):
-        scores=self.findScore(board)
-        high=self.highScores(scores)
-        q=len(high)-1
-        if self.tbt =='LEFT':
-            return high[0]
-        elif self.tbt=='RIGHT':
-            return high[q]
-        else:
-            return high[randint(0,q)]
-
-    # checks to see if move will lead to an immediate loss
-    def foresight(self,board,c):
-        for i in range(0,board.width):
-            l = board.addMove(self, c, self.ox)
-            l.addMove(self,i,self.opp)
-            if l.scoreBoard(self)==0:
-                return False
-        else:
-            return True
-
-    def nextMove(self,board):
-        return self.tiebreakMove(board)
-
 class Board:
     """ a datatype representing a C4 board
         with an arbitrary number of rows and cols
@@ -211,4 +139,79 @@ class Board:
                   x=True
             print self
 
+
+
+class Player:
+    def __init__(self, ox, tbt, ply):
+        self.ox=ox
+        self.tbt=tbt
+        self.ply=ply
+
+    def __repr__(self):
+        """ creates an appropriate string """
+        s = "Player for " + self.ox + "\n"
+        s += "  with tiebreak type: " + self.tbt + "\n"
+        s += "  and ply == " + str(self.ply) + "\n\n"
+        return s
+    def opp(self):
+        if self.ox=='X':
+            return 'O'
+        elif self.ox=='O':
+            return 'X'
+
+    def scoreBoard(self, Board):
+        z=self.opp()
+        if Board.winsFor(self.ox)==True:
+            return 100
+        elif Board.winsFor(z)==True:
+            return 0
+        elif Board.isFull()==True:
+            return 1
+        else:
+            return 50
+
+    def findScore(self,Board):
+        scores=[]
+        for Col in range(0,Board.width):
+            b=Board
+            b.addMove(Col,self.ox)
+            scores.append(self.scoreBoard(Board))
+        return scores
+    def highScores(self,scores,board):
+        k=[]
+        b=0
+        for i in range(0,len(scores)):
+            if scores[i]>b and self.foresight(board,i):
+                b=scores[i]
+                k=[]
+
+                k.append(i)
+            elif scores[i]==b and self.foresight(board,i):
+                k.append(i)
+        return k
+
+    def tiebreakMove(self,board):
+        scores=self.findScore(board)
+        high=self.highScores(scores,board)
+        q=len(high)-1
+        if self.tbt =='LEFT':
+            return high[0]
+        elif self.tbt=='RIGHT':
+            return high[q]
+        else:
+            return high[randint(0,q)]
+
+    # checks to see if move will lead to an immediate loss
+    def foresight(self, board ,c):
+        l=Board(0,0)
+        for i in range(0,board.width):
+            l = board.addMove(c, self.ox)
+            l.addMove(self,i,self.opp)
+            if l.scoreBoard(self)==0:
+                return False
+        else:
+            return True
+
+    def nextMove(self,board):
+        return self.tiebreakMove(board)
 
