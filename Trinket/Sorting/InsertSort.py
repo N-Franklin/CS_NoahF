5c5378d1b6991190d70f0ba3fc5489c0aa84f050
N-Franklin <nmfranklin@gmail.com>
2017-04-17 15:28:12 -0700

    InsertSort complete

---
+++ Trinket/Sorting/InsertSort.py
@@ -0,0 +1,16 @@
+def insertSort(a):
+    l=len(a)
+    v=0
+    for p in range(1,l,1):
+        if a[p]<a[p-1]:
+            v=a[p]
+            a[p]=a[p-1]
+            a[p-1]=v
+            print a
+            insertSort(a)
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+l=insertSort(alist)
+print(l)
+
+
