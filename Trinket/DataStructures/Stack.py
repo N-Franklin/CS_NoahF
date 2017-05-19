5e65a0cc6d8de04c08493d0f9b49c452b1ba17a0
N-Franklin <nmfranklin@gmail.com>
2017-05-19 11:06:12 -0700

    Quicksort still broken

---
+++ Trinket/DataStructures/Stack.py
@@ -0,0 +1,23 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+def parChecker(l):
+    c = Stack()
+    for i in (len(l)):
+        break;
--- Trinket/Sorting/Quick Sort.py
+++ Trinket/Sorting/Quick Sort.py
@@ -1,27 +1,55 @@
 from random import randint
+'''
+def swap(list,pivot,start,stop,pivoti):
+    Thing1=findLeft(list,pivot,start,stop,pivoti)
+    Thing2=findRight(list,pivot,start,stop,pivoti)
+    list[Thing1],list[Thing2]=list[Thing2],list[Thing1]
+    if Thing1==pivoti:
+        pivoti=Thing2
+    elif Thing2==pivoti:
+        pivoti=Thing1
 
 
-def quicksort(list,start,stop):
-    if start>stop:
-        return list
+def findLeft(list,pivot,start,stop,pivoti):
+    for index in (start, pivoti+1,1):
+        if list[index]>=pivot:
+            return index;
+    return 0;
+
+def findRight(list,pivot,start,stop,pivoti):
+    for index in (start, pivoti-1,1):
+        if list[index]<=pivot:
+            return index;
+    return 0;
+
+def QuickSort(list,start,stop):
+    pivoti=start
+    if start<stop:
+        pivot=list[start]
+        swap(list,pivot,start,swap,pivoti)
+'''
+
+def QuickSort(list,start,stop):
+    if start>=stop:
+        i=0;
     else:
         pivot = list[start]
         left = start
-        right = stop
-        while left <= right:
+        right = stop-1
+        while left < right:
             while list[left] < pivot:
                 left += 1
-            while list[right] > pivot:
+            while list[right] >= pivot:
                 right -= 1
 
-            if left <= right:
-                list[left], list[right] = list[right], list[left]
-                left += 1
-                right -= 1
-                print('So the list becomes:')
-                print(list)
-    quicksort(list, start, right)
-    quicksort(list, left, stop)
+            list[left], list[right] = list[right], list[left]
+            left += 1
+            right -= 1
+            print('So the list becomes:')
+            print(list)
+            print str(right)+','+str(left)
+        QuickSort(list, start, right)
+        QuickSort(list, left, stop)
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-l=quicksort(alist,0,8)
+l=QuickSort(alist,0,9)
 print(l)
