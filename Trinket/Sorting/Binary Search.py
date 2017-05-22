4c1a98745a12b59d183e0c443b61612d7bb67023
N-Franklin <nmfranklin@gmail.com>
2017-05-22 15:13:15 -0700

    added conditionals to binary search so it doesn't index out of bounds

--- Trinket/Sorting/Binary Search.py
+++ Trinket/Sorting/Binary Search.py
@@ -3,8 +3,15 @@ def binarySearch(list,int,start,stop):
     if list[mid]==int:
         return mid
     elif list[mid]>int:
-        return binarySearch(list,int,start,mid)
+        if mid==0:
+            return False
+        else:
+            return binarySearch(list,int,start,mid)
     elif list[mid]<int:
-        return binarySearch(list,int,mid,stop)
+        if mid==len(list)-1:
+            return False
+        else:
+            return binarySearch(list,int,mid,stop)
+
 q=[17, 20, 26, 31, 44, 54, 55, 77, 93]
 print binarySearch(q,20,0,8)
